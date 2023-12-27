a81f3c38354a7fb27ed30aa2d360c48e49f82205
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2023-12-27 13:11:39 +0800

    update login register Co-authored-by: willie-lin <yyhmmwan@gmail.com>

--- web/terminal-dashboard/src/dashboard/components/LoginForm.js
+++ web/terminal-dashboard/src/dashboard/components/LoginForm.js
@@ -1,10 +1,32 @@
 // LoginForm.js
-import React from 'react';
-import {login} from "../../api/api";
+import React, { useState } from 'react';
+import {checkEmail, login} from "../../api/api";
 
 function LoginForm({ onLogin }) {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const [emailError, setEmailError] = useState('');// 添加一个新的状态来保存邮箱错误信息
+
+    const handleEmailChange = async (e) => {
+        const email = e.target.value;
+        setEmail(email);
+        try {
+            const exists = await checkEmail(email);
+            // setEmailError(exists ? 'Email already registered' : '');
+            setEmailError(exists ? '' : 'Email not registered');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getInputClass = () => {
+        if (email.length === 0) {
+            return "border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md";
+        }
+        return emailError ? "border-green-500 w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-green-500 focus:ring-1 rounded-md" : "border-red-500 w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-red-500 focus:ring-1 rounded-md";
+        // return emailError ? "border-red-500 w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-red-500 focus:ring-1 rounded-md" : "border-green-500 w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-green-500 focus:ring-1 rounded-md";
+    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -24,24 +46,27 @@ function LoginForm({ onLogin }) {
                     <span className="text-2xl font-light">Login to your account</span>
                     <div className="mt-4 bg-white shadow-md rounded-lg text-left">
                     <div className="text-sm font-semibold leading-6 text-white bg-indigo-600 hover:bg-indigo-700 px-4 py-2 rounded"></div>
-                    <div className="px-8 py-6">
-                        <label className="block font-semibold mb-2">Email</label>
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-                               className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md"/>
-                        <label className="block mt-3 font-semibold mb-2">Password</label>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
-                               className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md"/>
-                        <div className="flex justify-between items-baseline">
-                            <button type="submit"
-                                // className="mt-4 bg-purple-600 text-white py-2 px-6 rounded-md hover:bg-purple-700">Login
-                                    className="mt-4 text-sm font-semibold leading-6 text-white bg-indigo-600 hover:bg-indigo-700 px-4 py-2 rounded">Login
-                            </button>
-                            <a href="#" className="text-sm hover:underline">Forgot password?</a>
+                        <div className="px-8 py-6">
+                            <label className="block font-semibold mb-2">Email</label>
+                            <input type="email" value={email} onChange={handleEmailChange}
+                                   className={getInputClass()}/>
+                            {emailError && <p className="text-red-500">{emailError}</p>}
+                            {/*<input type="email" value={email} onChange={(e) => setEmail(e.target.value)}*/}
+                            {/*       className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md"/>*/}
+                            <label className="block mt-3 font-semibold mb-2">Password</label>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                                   className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md"/>
+                            <div className="flex justify-between items-baseline">
+                                <button type="submit"
+                                    // className="mt-4 bg-purple-600 text-white py-2 px-6 rounded-md hover:bg-purple-700">Login
+                                        className="mt-4 text-sm font-semibold leading-6 text-white bg-indigo-600 hover:bg-indigo-700 px-4 py-2 rounded">Login
+                                </button>
+                                <a href="#/" className="text-sm hover:underline">Forgot password?</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </form>
     );
 }
