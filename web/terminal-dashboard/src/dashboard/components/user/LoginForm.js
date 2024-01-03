91b5b0a56fdd6b385c575940360ec4085d711d33
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2024-01-03 17:29:54 +0800

    update form

--- web/terminal-dashboard/src/dashboard/components/user/LoginForm.js
+++ web/terminal-dashboard/src/dashboard/components/user/LoginForm.js
@@ -215,7 +215,7 @@ function LoginForm({ onLogin }) {
                     </div>
                     <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
                         Not registered?
-                        <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
+                        <Link to="/register" className="text-gray-900 ml-1">Create account</Link>
                     </Typography>
 
 
--- web/terminal-dashboard/src/layout/Navigation.js
+++ web/terminal-dashboard/src/layout/Navigation.js
@@ -12,53 +12,53 @@ const Navigation = ({ isLoggedIn, onLogout }) => {
                         <span className="sr-only">Your Company</span>
                         <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
                     </Link>
-                    <div className="flex space-x-2">
-                        {isLoggedIn ? (
-                            <>
-                                <Button
-                                    color="lightBlue"
-                                    buttonType="filled"
-                                    size="regular"
-                                    rounded={false}
-                                    block={false}
-                                    iconOnly={false}
-                                    ripple="light"
-                                    onClick={onLogout}
-                                >
-                                    Logout
-                                </Button>
-                            </>
-                        ) : (
-                            <>
-                                <Link to="/login">
-                                    <Button
-                                        color="lightBlue"
-                                        buttonType="filled"
-                                        size="regular"
-                                        rounded={false}
-                                        block={false}
-                                        iconOnly={false}
-                                        ripple="light"
-                                    >
-                                        Log in
-                                    </Button>
-                                </Link>
-                                <Link to="/register">
-                                    <Button
-                                        color="lightBlue"
-                                        buttonType="filled"
-                                        size="regular"
-                                        rounded={false}
-                                        block={false}
-                                        iconOnly={false}
-                                        ripple="light"
-                                    >
-                                        Register
-                                    </Button>
-                                </Link>
-                            </>
-                        )}
-                    </div>
+                    {/*<div className="flex space-x-2">*/}
+                    {/*    {isLoggedIn ? (*/}
+                    {/*        <>*/}
+                    {/*            <Button*/}
+                    {/*                color="lightBlue"*/}
+                    {/*                buttonType="filled"*/}
+                    {/*                size="regular"*/}
+                    {/*                rounded={false}*/}
+                    {/*                block={false}*/}
+                    {/*                iconOnly={false}*/}
+                    {/*                ripple="light"*/}
+                    {/*                onClick={onLogout}*/}
+                    {/*            >*/}
+                    {/*                Logout*/}
+                    {/*            </Button>*/}
+                    {/*        </>*/}
+                    {/*    ) : (*/}
+                    {/*        <>*/}
+                    {/*            <Link to="/login">*/}
+                    {/*                <Button*/}
+                    {/*                    color="lightBlue"*/}
+                    {/*                    buttonType="filled"*/}
+                    {/*                    size="regular"*/}
+                    {/*                    rounded={false}*/}
+                    {/*                    block={false}*/}
+                    {/*                    iconOnly={false}*/}
+                    {/*                    ripple="light"*/}
+                    {/*                >*/}
+                    {/*                    Log in*/}
+                    {/*                </Button>*/}
+                    {/*            </Link>*/}
+                    {/*            <Link to="/register">*/}
+                    {/*                <Button*/}
+                    {/*                    color="lightBlue"*/}
+                    {/*                    buttonType="filled"*/}
+                    {/*                    size="regular"*/}
+                    {/*                    rounded={false}*/}
+                    {/*                    block={false}*/}
+                    {/*                    iconOnly={false}*/}
+                    {/*                    ripple="light"*/}
+                    {/*                >*/}
+                    {/*                    Register*/}
+                    {/*                </Button>*/}
+                    {/*            </Link>*/}
+                    {/*        </>*/}
+                    {/*    )}*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
