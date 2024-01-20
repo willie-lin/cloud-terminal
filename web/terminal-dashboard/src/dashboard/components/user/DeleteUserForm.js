14f863d50f16185cc0d4a4e7e3997759ddbebb4e
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2024-01-20 22:27:59 +0800

    add update user

---
+++ web/terminal-dashboard/src/dashboard/components/user/DeleteUserForm.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {Button, Card, CardBody, CardHeader, Input, Typography} from "@material-tailwind/react";
+
+function DeleteUserForm({ user, onDeleteUser }) {
+    const [modalOpen, setModalOpen] = useState(false);
+    // 使用user的值来初始化你的状态
+    const [username, setUsername] = useState(user ? user.username : '');
+
+
+    function handleSubmit(e) {
+        e.preventDefault();  // 阻止表单的默认提交行为
+        if (window.confirm('Are you sure you want to delete this user?')) {
+            // Your delete logic here
+        }
+        try {
+
+            onDeleteUser()
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
+    return (
+        <Card className="w-96">
+            <CardHeader variant="gradient" color="gray" className="mb-4 grid h-20 place-items-center">
+                <Typography variant="h4" color="white">
+                    Delete User
+                </Typography>
+            </CardHeader>
+            <CardBody className="px-4 py-4">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-1 flex flex-col gap-6">
+                        <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+                            ID
+                        </Typography>
+                        <Input
+                            variant="outlined"
+                            label="Disabled"
+                            disabled
+                            type="username"
+                            color="lightBlue"
+                            size="regular"
+                            outline={true}
+                            value={username}
+                            name="username"  // 添加name属性
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-between">
+                        <Button
+                            color="lightBlue"
+                            buttonType="filled"
+                            size="regular"
+                            rounded={false}
+                            block={false}
+                            iconOnly={false}
+                            ripple="light"
+                            type="submit"
+                        >
+                            确定
+                        </Button>
+                        <Button
+                            color="red"
+                            buttonType="filled"
+                            size="regular"
+                            rounded={false}
+                            block={false}
+                            iconOnly={false}
+                            ripple="light"
+                            type="button"
+                            // onClick={() => setModalOpen(false)}
+                            onClick={() => window.close()}
+                        >
+                            取消
+                        </Button>
+                    </div>
+                </form>
+            </CardBody>
+        </Card>
+    )
+        ;
+}
+
+export default DeleteUserForm;
--- web/terminal-dashboard/src/dashboard/components/user/UserRow.js
+++ web/terminal-dashboard/src/dashboard/components/user/UserRow.js
@@ -1,16 +1,15 @@
 import {Avatar, Chip, IconButton, Tooltip, Typography} from "@material-tailwind/react";
-import {PencilIcon} from "@heroicons/react/16/solid";
+import {PencilIcon, TrashIcon} from "@heroicons/react/16/solid";
 import UpdateUser from "./UpdateUser";
 import {useState} from "react";
+import DeleteUserForm from "./DeleteUserForm";
 
 
 function UserRow({ user, isLast }) {
     const classes = isLast ? "p-2" : "p-2 border-b border-blue-gray-50";
-    // 假设你有一个状态来存储当前正在编辑的用户
+
     const [editingUser, setEditingUser] = useState(null);
-
     const [isUpdateUserOpen, setIsUpdateUserOpen] = useState(false);
-
     const handleUpdateUser = () => {
         setIsUpdateUserOpen(false);
     };
@@ -18,136 +17,179 @@ function UserRow({ user, isLast }) {
         setEditingUser(user)
         setIsUpdateUserOpen(true);
     };
-
     const closeUpdateUser = () => {
         setIsUpdateUserOpen(false);
     };
 
+    const [deleteUser, setDeleteUser] = useState(null);
+    const [isDeleteUserOpen, setIsDeleteUserOpen] = useState(false);
+
+    const handleDeleteUser = () => {
+        setIsDeleteUserOpen(false);
+    };
+
+    function openDeleteUser(user) {
+        setDeleteUser(user)
+        setIsDeleteUserOpen(true)
+    }
+
+    function closeDeleteUser() {
+        setIsDeleteUserOpen(false);
+    }
+
     return (
         <>
-        <tr key={user.id}>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.id}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex items-center gap-3">
-                    {/* 这里可以添加Avatar组件 */}
-                    <Avatar src={user.avatar} alt={user.nickname} size="sm"/>
+            <tr key={user.id}>
+                <td className={classes}>
                     <div className="flex flex-col">
-                        <Typography variant="small" color="blue-gray"
-                                    className="font-normal">
-                            {user.nickname}
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.id}
                         </Typography>
-                        <Typography variant="small" color="blue-gray"
-                                    className="font-normal">
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex items-center gap-3">
+                        {/* 这里可以添加Avatar组件 */}
+                        <Avatar src={user.avatar} alt={user.nickname} size="sm"/>
+                        <div className="flex flex-col">
+                            <Typography variant="small" color="blue-gray"
+                                        className="font-normal">
+                                {user.nickname}
+                            </Typography>
+                            <Typography variant="small" color="blue-gray"
+                                        className="font-normal">
+                                {user.email}
+                            </Typography>
+                        </div>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.username}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
                             {user.email}
                         </Typography>
                     </div>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.username}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.email}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.phone}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.bio}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.totp_secret}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="w-max">
-                    <Chip variant="ghost" size="sm" value={user.online ? "在线" : "离线"}
-                          color={user.online ? "green" : "blue-gray"}
-                    />
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.enable_type ? '启用' : '禁用'}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.created_at}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.updated_at}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <div className="flex flex-col">
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                        {user.last_login_time}
-                    </Typography>
-                </div>
-            </td>
-            <td className={classes}>
-                <Tooltip content="Edit User" placement="top">
-                    <IconButton variant="text"
-                        color="lightBlue"
-                        buttonType="filled"
-                        size="regular"
-                        rounded={false}
-                        block={false}
-                        iconOnly={true}
-                        ripple="light"  // dark
-                        onClick={() => openUpdateUser(user)}
-                    >
-                        <PencilIcon className="h-4 w-4" />
-                    </IconButton>
-                </Tooltip>
-            </td>
-        </tr>
-    {isUpdateUserOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
-             onClick={(e) => {
-                 // 如果事件的目标是这个容器本身，那么关闭模态窗口
-                 if (e.target === e.currentTarget) {
-                     closeUpdateUser();
-                 }
-             }}
-        >
-            <UpdateUser user={editingUser} onUpdateUser={handleUpdateUser}  onClose={closeUpdateUser}/>
-        </div>
-    )}
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.phone}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.bio}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.totp_secret}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="w-max">
+                        <Chip variant="ghost" size="sm" value={user.online ? "在线" : "离线"}
+                              color={user.online ? "green" : "blue-gray"}
+                        />
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.enable_type ? '启用' : '禁用'}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.created_at}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.updated_at}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {user.last_login_time}
+                        </Typography>
+                    </div>
+                </td>
+                <td className={classes}>
+                    <Tooltip content="Edit User" placement="top">
+                        <IconButton variant="text"
+                                    color="lightBlue"
+                                    buttonType="filled"
+                                    size="regular"
+                                    rounded={false}
+                                    block={false}
+                                    iconOnly={true}
+                                    ripple="light"  // dark
+                                    onClick={() => openUpdateUser(user)}
+                        >
+                            <PencilIcon className="h-4 w-4"/>
+                        </IconButton>
+                    </Tooltip>
+                    {isUpdateUserOpen && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+                             onClick={(e) => {
+                                 // 如果事件的目标是这个容器本身，那么关闭模态窗口
+                                 if (e.target === e.currentTarget) {
+                                     closeUpdateUser();
+                                 }
+                             }}
+                        >
+                            <UpdateUser user={editingUser} onUpdateUser={handleUpdateUser}  onClose={closeUpdateUser}/>
+                        </div>
+                    )}
+                </td>
+                <td className={classes}>
+                    <Tooltip content="Delete User" placement="top">
+                        <IconButton variant="text"
+                                    color="red"
+                                    buttonType="filled"
+                                    size="regular"
+                                    rounded={false}
+                                    block={false}
+                                    iconOnly={true}
+                                    ripple="light"
+                                    onClick={() => openDeleteUser(user)}
+                        >
+                            <TrashIcon className="h-4 w-4"/>
+                        </IconButton>
+                    </Tooltip>
+                    {isDeleteUserOpen && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+                             onClick={(e) => {
+                                 // 如果事件的目标是这个容器本身，那么关闭模态窗口
+                                 if (e.target === e.currentTarget) {
+                                     closeDeleteUser();
+                                 }}}
+                        >
+                        <DeleteUserForm user={deleteUser} onDeleteUser={handleDeleteUser} onclose={closeDeleteUser}/>
+                        </div>
+                    )}
+                </td>
+            </tr>
+
     </>
     );
 }
